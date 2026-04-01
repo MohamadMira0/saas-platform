9964bf4332a72e1f48032528f1bd9ae725ea680f
MohamadMira0 <mohamad.y.mira@gmail.com>
2026-04-01 16:03:12 +0300

    update js

--- html/home.js
+++ html/home.js
@@ -261,13 +261,303 @@
 // let result = sum(5, 15) + 10;
 // console.log(result);
 
-let numbers = [10, 20, 25, 30, 35];
+// let numbers = [10, 20, 25, 30, 35, 100, 200, 250, 300, 350];
 // for (let i = 0; i < numbers.length; i++) {
 //   console.log(i);
 //   console.log(numbers[i]);
 // }
 
-for (let i = 0; i < numbers.length; i++) {
-  console.log(i);
-  console.log(numbers[i]);
-}
+// for (number of numbers) {
+//   console.log(number);
+// }
+
+// for (let i = 0; i < numbers.length; i++) {
+//   console.log(numbers[i]);
+// }
+
+// let i = 0;
+// while (i < numbers.length - 4) {
+//   console.log(numbers[i]);
+//   i++;
+// }
+
+// do {
+//   console.log(numbers[i]);
+//   i++;
+// } while (i < 9);
+
+// let message = "                 Hello             World!                 ";
+
+// console.log(message.length);
+
+// console.log(message.slice(0, -1));
+// let newMessage = message.split(" ");
+// console.log(newMessage);
+// console.log(newMessage.join("."));
+
+// let newMess = message.substring(0, 5);
+
+// console.log(newMess.substring(0, -1));
+// console.log(message.replace("H", "M"));
+// console.log(message.repeat(3));
+// console.log(message.repeat(3));
+// console.log(message);
+// console.log(message.trim().toUpperCase().toLowerCase());
+
+// function even(num) {
+//   return num % 2 === 0; console.log();3
+
+// }
+
+// even(4)
+
+// let x = 5.8;
+
+// console.log(Math.ceil(x));
+// console.log(Math.floor(x));
+// console.log(Math.round(x));
+// console.log(Math.round(Math.random() * 1000000000));
+
+// console.log(Math.max(5, 4, 7, 15));
+// console.log(Math.min(5, 4, 7, 15, 1));
+// console.log(Math.pow(2, 3));
+// console.log(Math.sqrt(9, 2));
+
+// for (number of numbers) {
+//   console.log(number);
+// }
+
+// let numbers = [10, 20, 25, 30, 35];
+// for (let i = 0; i < numbers.length; i++) {
+//   console.log(numbers[i]);
+// }
+
+// let i = 0;
+// while (i < 0) {
+//   console.log(numbers[i]);
+//   i++;
+//   console.log(`i = ${i}`);
+// }
+
+// do {
+//   console.log(numbers[i]);
+//   i++;
+//   console.log(`i = ${i}`);
+// } while (i < 2);
+
+// let message = "Hello,World,My Name,Mohammed!";
+// console.log(message.length);
+// let newMessage = message.slice(0, -4);
+// console.log(newMessage);
+// let newMess = message.substring(0, -4);
+// console.log(newMess);
+
+// let splitMessage = message.split(",");
+// console.log(message.split(","));
+// console.log(splitMessage.join(" "));
+
+// console.log(message.replace("h", "N"));
+// console.log(message.toUpperCase());
+// console.log(message.toLowerCase());
+
+// function even(number) {
+//   return number % 2 == 0;
+// }
+// let x = 4.97436;
+
+// console.log(x.toFixed(2));
+// console.log(Math.ceil(x));
+// console.log(Math.floor(x));
+// console.log(Math.round(x));
+// console.log(Math.max(10, 202, 30));
+// console.log(Math.min(10, -202, 30));
+// console.log(Math.abs(-5));
+// console.log(Math.pow(2, 3));
+// console.log(Math.sqrt(4));
+
+// console.log(Math.pow(Math.round(x), 2));
+
+// let x = 155;
+// let y = 15;
+// let z = 15;
+
+// if (x >= y || x > z) {
+//   console.log("x large");
+// } else if (x !== y) {
+//   console.log("y is equal x");
+// } else {
+//   console.log("y large");
+// }
+
+// let cond = x < y ? (x = 10) : x == z ? "x = z" : y == x ? ""  : "";
+// console.log(cond);
+
+// let global = "The Varible is global";
+
+// function num(a, b) {
+//   let local = "The Varible is local";
+//   console.log(global);
+//   console.log(local);
+//   console.log(a);
+// }
+
+// num(5);
+// console.log(local);
+
+// Todo DOM
+
+// let ele = document.getElementById("about");
+// console.log(ele);
+// let containerElements = document.getElementsByClassName("container");
+// console.log(typeof containerElements);
+// console.log(containerElements);
+
+// let ele = document.querySelector("#home");
+// ele.textContent = "New Home";
+// console.log(ele);
+
+// let x = 15;
+// let y = "15";
+// let z = 50;
+// if ((x = y && x != z)) console.log("x is small");
+// else if (y >= z) console.log("y < z");
+// else console.log(" y is small");
+
+// let cond =
+//   x < y && x > z
+//     ? console.log("x is small")
+//     : y > z
+//       ? console.log("y < z")
+//       : console.log(" y is small");
+// console.log(x < y);
+
+// if (x != y) console.log("true");
+// else console.log("false");
+// =  || == || ===
+
+// let global = "The Varibel is Global";
+
+// function typeVar(a, b) {
+//   let local = "The Varibel is Local";
+//   console.log(local);
+//   console.log(global);
+//   console.log(a * b);
+// }
+// function sum() {
+//   console.log(local);
+// }
+
+// sum();
+// typeVar(6, 5);
+// console.log(a * b);
+// console.log(local);
+// console.log(global);
+
+// let ele = document.getElementById("logo");
+// let ele = document.getElementsByClassName("container");
+// let ele1 = document.get("container");
+// let ele = document.querySelector(".container");
+// let ele = document.querySelectorAll("");
+// console.log(ele[0]);
+
+// function name(para1, para2) {}
+
+// let name1 = (para1, para2) => {};
+
+// TODO: immediately invoked function experssion
+// let x = 5;
+// let y = 10;
+// let res = 0;
+// (function name(a, b) {
+//   return (res = a + b);
+// })(x, y);
+
+// console.log(res);
+
+// let element = document.getElementsByTagName("h1")[0];
+// console.log(element);
+
+// element.setAttribute("class", "title");
+// console.log(element.hasAttribute("name"));
+// element.removeAttribute("name");
+// console.log(element.getAttribute("name"));
+
+// element.id = "title";
+// element.className = "hello ana class";
+// element.title = "hello ana class";
+
+// element.className += " memo";
+// element.classList.add("memo");
+// console.log(element.classList.contains("memo"));
+// element.classList.remove("memo");
+// console.log(element.classList.contains("memo"));
+// element.classList.replace("ana", "I'm");
+// console.log(element.classList);
+// element.addEventListener("click", () => {
+//   element.classList.toggle("active");
+// });
+// console.log(element.id);
+// array.forEach(element => {
+// });
+// array.map(element => {
+
+// });
+// element.style.backgroundColor = "blue";
+
+// let newElement = document.createElement("p");
+// newElement.innerHTML = "<p>Mohammed</p>";
+// newElement.textContent = "Mohammed";
+// newElement.innerHTML = "Mohammed";
+// newElement.innerText = "Mohammed";
+// console.log(newElement);
+// let bodyElement = document.getElementsByTagName("body")[0];
+// console.log(bodyElement);
+// bodyElement.append(newElement);
+// bodyElement.appendChild(newElement);
+
+// if (!isLogged) {
+
+// }
+
+// array.map(element => {
+
+// });
+// array.forEach(element => {
+
+// });
+
+// for(let i =0; i < 10; i++) {
+
+// }
+
+let headTitle = document.querySelector("h1");
+// console.log(headTitle);
+// console.log(headTitle);
+// headTitle.setAttribute("class", "Mostafa");
+// headTitle.setAttribute("id", "idendasd");
+// console.log(headTitle.getAttribute("name"));
+// console.log(headTitle.hasAttribute("name"));
+// console.log(headTitle.removeAttribute("name"));
+// console.log(headTitle.hasAttribute("name"));
+
+// let x = { id: 4, name: "nani" };
+
+// x.id = 6;
+
+// console.log(x);
+
+// headTitle.className += " rami";
+// headTitle.id = "noha";
+// headTitle.href = "noha";
+// headTitle.classList.add("nour");
+// headTitle.classList.contains("gass");
+// headTitle.classList.replace("rami", "ssssss");
+// headTitle.classList.remove("ssssss");
+// console.log(headTitle.classList.contains("gass"));
+// console.log(headTitle.classList);
+
+headTitle.addEventListener("click", () => {
+  headTitle.classList.toggle("active");
+  //   headTitle.style.color = "red";
+  //   headTitle.style.fontSize = "blue";
+});
